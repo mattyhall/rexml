0e29a87206e0b4866bed98f63b52900dbd41a809
Matt Hall <matthew@quickbeam.me.uk>
2022-08-20 12:13:49 +0100

    Build using nix

--- flake.nix
+++ flake.nix
@@ -9,8 +9,21 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
+
         rustVersion = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        rustBuild = rustPlatform.buildRustPackage {
+          pname = "rexml";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
       in {
+        defaultPackage = rustBuild;
         devShell = pkgs.mkShell {
           buildInputs =
             [ (rustVersion.override { extensions = [ "rust-src" ]; }) ] ++ (with pkgs; [
